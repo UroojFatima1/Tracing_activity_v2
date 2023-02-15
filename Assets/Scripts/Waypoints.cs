435277f64a7bfb1ba552224c3e514d572f2db7ca
UroojFatima1 <uroojfatima1518@gmail.com>
2023-02-15 20:51:49 +0500

    first commit

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Waypoints : MonoBehaviour
+{
+
+	[SerializeField]
+	Transform[] waypoints;
+
+	[SerializeField]
+	float moveSpeed = 2f;
+	public int waypointIndex;
+	public GameObject destroy_tracing;
+	public GameObject activate_tracing;
+	public GameObject Waypoint_object;
+	public GameObject changeText;
+	public bool tracing_two = false;
+	public GameObject tracing_script;
+	public GameObject prefab_ball;
+	Vector3 ballposition;
+	void Start()
+	{
+		transform.position = waypoints[waypointIndex].transform.position;
+		if (Waypoint_object.name == "clicking")
+			waypointIndex = 0;
+		else
+			waypointIndex = -1;
+		if (Waypoint_object.name == "SoccerBall") { ballposition = Waypoint_object.transform.position; };
+	}
+
+	void Update()
+	{
+		
+			Move();
+	}
+
+	public void Move()
+	{
+		
+		transform.position = Vector3.MoveTowards(transform.position,
+												waypoints[waypointIndex].transform.position,
+												moveSpeed * Time.deltaTime);
+
+		if (transform.position == waypoints[waypointIndex].transform.position)
+			waypointIndex += 1;
+		{
+		}
+
+		if (waypointIndex == waypoints.Length && Waypoint_object.name=="clicking")
+			waypointIndex = 0;
+
+		if (waypointIndex == waypoints.Length && Waypoint_object.name == "SoccerBall")
+		{
+			Destroy(destroy_tracing);
+			Waypoint_object.transform.parent.gameObject.SetActive(false);
+			activate_tracing.SetActive(true);
+			changeText.GetComponent<TextMeshProUGUI>().text = "TWO";
+			tracing_script.GetComponent<Audio>().tracing_two = true;
+			tracing_script.GetComponent<Audio>().tracefollowing();
+			Instantiate(prefab_ball, ballposition, Quaternion.identity);
+			Instantiate(prefab_ball, transform.position - Vector3.up+Vector3.left, Quaternion.identity);
+
+		}
+
+
+	}
+	
+
+}
+
+
